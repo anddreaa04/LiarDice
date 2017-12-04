66a6e368eb613f62c8a15a8d82231d10d0c27219
andreaortega <andreaortegadumont@gmail.com>
2017-12-04 17:10:40 +0100

    Afegim prova Bluetooth

---
+++ ProvaBluetooth/app/src/main/java/edu/upc/eseiaat/pma/liardice/HomeActivityBluetooth.java
@@ -0,0 +1,48 @@
+package edu.upc.eseiaat.pma.liardice;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class HomeActivityBluetooth extends AppCompatActivity {
+
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private int REQUEST_ENABLE_BT;
+    private ArrayList<String> mArrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_bluetooth);
+
+        //ADAPTADOR BLUETOOTH
+
+        if (mBluetoothAdapter==null) {
+            Toast.makeText(this, "Device does not support Bluetooth", Toast.LENGTH_LONG).show();
+        }
+        //HABILITAR BLUETOOTH
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT); //TODO: fer un OnActivityResult
+        }
+        /*
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+// If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }*/
+
+    }
+
+}
+
