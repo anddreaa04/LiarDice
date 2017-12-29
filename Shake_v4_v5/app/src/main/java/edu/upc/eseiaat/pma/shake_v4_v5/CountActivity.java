64cc66913e843a62f3bf3666bd18e30bceec31b2
andreaortega <andreaortegadumont@gmail.com>
2017-12-29 21:20:26 +0100

    Inicio versiones 4 y 5 LiarDice

---
+++ Shake_v4_v5/app/src/main/java/edu/upc/eseiaat/pma/shake_v4_v5/CountActivity.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.shake_v4_v5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count);
+    }
+}
